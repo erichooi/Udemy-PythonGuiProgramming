a36c2451f0d70c96b6fcce6d5cc4512beb772361
Eric Khoo Jiun Hooi <erichooi1995@gmail.com>
2017-08-18 13:10:56 +0800

    finish ExpressionEval.py

--- ExpressionEval.py
+++ ExpressionEval.py
@@ -1,15 +1,33 @@
 import sys
-from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5 import QtWidgets
 
 
-class Form(QDialog):
+class Form(QtWidgets.QDialog):
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
 
+        self.resultsList = QtWidgets.QTextBrowser()
+        self.resultsInput = QtWidgets.QLineEdit("Enter an expression and press return key")
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.resultsList)
+        layout.addWidget(self.resultsInput)
+        self.setLayout(layout)
+
+        self.resultsInput.selectAll()
+        self.resultsInput.setFocus()
+
+        self.resultsInput.returnPressed.connect(self.compute)
+
+    def compute(self):
+        try:
+            text = self.resultsInput.text()
+            self.resultsList.append("{0} = <b>{1}</b>".format(text, eval(text)))
+        except:
+            self.resultsList.append("<font color=red><b>Expression invalid</b></font>")
 
 
-
-app = QApplication(sys.argv)
+app = QtWidgets.QApplication(sys.argv)
 form = Form()
 form.show()
 app.exec_()
