72779e8832f432b67ec30684e51bf454549e9e6f
Eric Khoo Jiun Hooi <erichooi1995@gmail.com>
2017-08-19 00:34:27 +0800

    Signal.py done

---
+++ Signals.py
@@ -0,0 +1,25 @@
+from PyQt5 import QtWidgets
+from urllib import request
+import sys
+
+class Form(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+
+        self.dial = QtWidgets.QDial()
+        self.dial.setNotchesVisible(True)
+
+        self.spinbox = QtWidgets.QSpinBox()
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.dial)
+        layout.addWidget(self.spinbox)
+        self.setLayout(layout)
+
+        self.dial.valueChanged.connect(self.spinbox.setValue)
+        self.spinbox.valueChanged.connect(self.dial.setValue)
+
+app = QtWidgets.QApplication(sys.argv)
+form = Form()
+form.show()
+app.exec_()
